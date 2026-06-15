2e8729a9cdc068e09dfdfd169cdb10fb5357b4f7
Anusha Bollimuntha <bollimunthaanusha1@gmail.com>
2026-06-15 12:19:10 +0530

    fix mongoose user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -23,22 +24,27 @@ const userSchema = new mongoose.Schema(
       enum: ["admin", "trainer", "student"],
       default: "student",
     },
+
     enrolledCourses: [
-  {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-  },
-],
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+
   next();
 });
-module.exports = mongoose.model("User", userSchema);
+
+module.exports =
+  mongoose.models.User || mongoose.model("User", userSchema);
